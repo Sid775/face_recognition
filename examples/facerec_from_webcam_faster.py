3f18480722e875430925928044d1c95a8e145367
Sid775 <sid775@mail.ru>
2019-06-02 12:17:08 +0300

    added docker file customise web cam util

--- examples/facerec_from_webcam_faster.py
+++ examples/facerec_from_webcam_faster.py
@@ -18,17 +18,22 @@ obama_image = face_recognition.load_image_file("obama.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("biden.jpg")
+biden_image = face_recognition.load_image_file("me.jpeg")
 biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+
+biden_image_1 = face_recognition.load_image_file("tema.jpg")
+biden_face_encoding_1 = face_recognition.face_encodings(biden_image_1)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
     obama_face_encoding,
-    biden_face_encoding
+    biden_face_encoding,
+    biden_face_encoding_1
 ]
 known_face_names = [
     "Barack Obama",
-    "Joe Biden"
+    "Ivan",
+    "Artem"
 ]
 
 # Initialize some variables
